dc7697f24a7b2303723c121d49a7ba5845a5fa10
Edward Garson <egarson@gmail.com>
2013-06-03 17:39:50 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MedianPriceRails::Application.config.secret_token = '76d82496276cd3f39de5e564bf90e271e0d91dc7c6691eabfcf936ad00387db183d246038d04c9a3c955142b523f1c79d8078fde23d968caffc5b738b8ca7222'
