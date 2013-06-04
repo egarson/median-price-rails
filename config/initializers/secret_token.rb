68a77312927b87fa959682d3a362ab4cc31eb911
Edward Garson <egarson@gmail.com>
2013-06-03 20:22:12 -0400

    configure app for postgres in a DRY manner

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-MedianPriceRails::Application.config.secret_token = '76d82496276cd3f39de5e564bf90e271e0d91dc7c6691eabfcf936ad00387db183d246038d04c9a3c955142b523f1c79d8078fde23d968caffc5b738b8ca7222'
+MedianPriceRails::Application.config.secret_token = '1ae500946cfc6dc3688102c10220f7ae110f1d28e0827eff5ebd6415b2a545824f54c0e9e2bcd9b8b55fc012cb252f63031ce661e6501326807d1d38d7084e1f'
