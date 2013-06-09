0c6da6af54a11fdc750febe6dc2d272fe4fc2e57
Edward Garson <egarson@gmail.com>
2013-06-09 16:38:22 -0400

    pivot: delete sales (just merge with item)

--- spec/models/sale_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-describe Sale do
-  it 'has item_id and paid attributes' do
-	s = Sale.new(:item_id => 1, :paid => 2.34)
-	s.item_id.should eq 1
-	s.paid.should eq 2.34
-  end
-end
